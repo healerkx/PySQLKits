ca6a4e3d455eb0cee9edefcfef50f08dfd607791
healer <healer.kx.yu@gmail.com>
2016-05-31 17:22:01 +0800

    Render data in HTML with tornado.Template

--- scripts/datachain/table_data.py
+++ scripts/datachain/table_data.py
@@ -3,15 +3,31 @@ import sys
 sys.path.append('D:\\Projects\\PySQLKits\\lib\\simplequery')
 
 from simplequery import *
-
+from tornado.template import Template
 
 def table_data(file, *data):
-    pass
+    with open('table_data.html', 'r', encoding='utf-8') as r:
+        content = r.read()
+    
+    datasets = []
+    d = {}
+    for item in data:
+        if isinstance(item, Handle):
+            v = item.get_value()
+            datasets.append(list(v))
+            print("DDDD~~~")
+            print(v)
+    d['datasets'] = datasets
+    t = Template(content)
+    c = t.generate(**d)
+
+    file.write(c.decode('utf-8'))
+    
 
 
 @buildin
 def t(handle, *data):
-    print("$$$" * 40)
+    
     f = handle.get_value()
     if f is not None:
         table_data(f, *data)
